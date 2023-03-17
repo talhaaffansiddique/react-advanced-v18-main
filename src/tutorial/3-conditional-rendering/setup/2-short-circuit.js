2ccb985e881a2c200ea8714717080982a247d889
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-03-17 18:35:08 +0500

    55 - Ternary Operator

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("k");
 
-  return <h2>short circuit</h2>;
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+
+  return (
+    <>
+      <h1>{firstValue}</h1>
+      <h1>Value : {secondValue}</h1>
+    </>
+  );
 };
 
 export default ShortCircuit;
